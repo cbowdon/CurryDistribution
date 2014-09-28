519451d9eac0156792202315f9b8cd850ee84749
Chris Bowdon <c.bowdon@bath.edu>
2014-09-28 22:45:15 +0100

    Stripped out story comments, actually getting down to it.

--- Main.hs
+++ Main.hs
@@ -1,54 +1,94 @@
-{-
- - We're going to approach this with type-first programming. That is, we use the type system to design our program.
- - One of the best features of Haskell is 'undefined'. It's a dummy definition that will throw a runtime error.
- - Sounds useless, but is actually very useful; it allows you to check the types compile without implementing anything.
- -}
-main :: IO ()
-main = undefined
-
-{-
- - It's a turn-based game, so there will be a board that is updated on every input from the player.
- - There will be a data structure to contain the state of the board.
- - The data we care about is the turn number and the state of the supply chain, which is a list of players.
- -}
 data Board = Board { turn :: Int
                    , chain :: [Player]
                    } deriving Show
 
-{-
- - There will also be a function that updates the board on each turn.
- - Its inputs will be the current board, the Customer's order and the player's input (their order).
- -}
-updateBoard :: Board -> Int -> Int -> Board
-updateBoard = undefined
+type History = [Board]
 
-{-
- - The player has an associated inventory and backlog.
- - They have a position in the supply chain.
- - They may not be human.
- -}
-data Player = Player { backlog :: Int
-                     , inventory :: Int
+data Player = Player { inventory :: Int
                      , isHuman :: Bool
                      , position :: Position
+                     , orders :: [Int]
                      } deriving Show
 
-{-
- - Position may not actually matter since every player in the chain has the same behaviour except those at the ends.
- - We may even want to experiment with chains of arbitrary length. Perhaps we will redefine this later.
- -}
-data Position = Customer | Retailer | Wholesaler | Distributor | Factory
+data Position = Customer |
+                Retailer |
+                Wholesaler |
+                Distributor |
+                Factory |
+                Robots deriving Show
 
-{-
- - There will be a function to update a player with an imminent arrival and order.
- - It will deduct from inventory and add to backlog as appropriate.
- -}
-updatePlayer :: Player -> Int -> Int -> Player
-updatePlayer :: undefined
+data PlayerInput = Order Int | QuitGame deriving Show
 
-{-
- - The challenging part is representing the time-delayed orders.
- - Remember, upstream players only receive the order after 1 turn, and the downstream player only receives the product after 2.
- - Perhaps a better way to model the entire game would be as a sequence of events updating the board.
- - The current state of the board can be derived by applying the events. This is known as "event sourcing".
- -}
+initialOrders = [0, 0]
+
+initialPlayers = [ Player { inventory = 100
+                          , isHuman = False
+                          , position = Factory
+                          , orders = initialOrders }
+                 , Player { inventory = 100
+                          , isHuman = False
+                          , position = Distributor
+                          , orders = initialOrders }
+                 , Player { inventory = 100
+                          , isHuman = False
+                          , position = Distributor
+                          , orders = initialOrders }
+                 , Player { inventory = 100
+                          , isHuman = True
+                          , position = Wholesaler
+                          , orders = initialOrders }
+                 , Player { inventory = 100
+                          , isHuman = False
+                          , position = Retailer
+                          , orders = initialOrders }
+                 , Player { inventory = 100
+                          , isHuman = False
+                          , position = Customer
+                          , orders = initialOrders } ]
+
+initialBoard = Board { turn = 0, chain = initialPlayers }
+
+main :: IO ()
+main = do
+    b <- generateBoard
+    b' <- runTurn [b]
+    print b'
+
+runTurn :: History -> IO History
+runTurn h = do
+    co <- getCustomerOrder
+    pi <- getPlayerOrder
+    case pi of
+        QuitGame -> return h
+        Order pi -> runTurn $  updateBoard h co pi : h
+
+generateBoard :: IO Board
+generateBoard = undefined
+
+getCustomerOrder :: IO Int
+getCustomerOrder = undefined
+
+getPlayerOrder :: IO PlayerInput
+getPlayerOrder = undefined
+
+-- customer to factory
+subtractOrders :: [Player] -> [Player]
+subtractOrders (p1:p2:ps) = subtractOrder p1 p2 : subtractOrders (p2:ps)
+subtractOrders (p:_)      = [p]
+
+-- Subtract p1's order from 2 turns ago from p2
+subtractOrder :: Player -> Player -> Player
+subtractOrder = moveProduct (-)
+
+type AddOp = Int -> Int -> Int
+
+moveProduct :: AddOp -> Player -> Player -> Player
+moveProduct op p1 p2 = case orders p1 of
+    o:_ -> p2 { inventory = inventory p2 `op` o }
+    _   -> p2
+
+dropOldestOrder :: Player -> Player
+dropOldestOrder p = p { orders = drop 1 $ orders p }
+
+updateBoard :: History -> Int -> Int -> Board
+updateBoard = undefined
